ce69c3827dbf8254f1c172ae51f8d15349b0fa76
Necdet Can Ateşman <c.atesman@gmx.net>
2016-04-21 13:37:29 +0200

    Release: 0.5.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.auth',
-    version='0.5.3',
+    version='0.5.4',
     description='Authorization and Authentication for The SCORE Framework',
     long_description=README,
     author='strg.at',
